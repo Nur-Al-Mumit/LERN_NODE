f9c9d39c304f7c749f25cae41dd7210e86935b67
nuralmumit <nuralmumit@nuralmumit.nuralmumit>
2024-05-25 11:43:10 +0600

    start of rest api

--- server2.js
+++ server2.js
@@ -3,24 +3,33 @@ import { createServer } from 'http';
 const PORT = process.env.PORT
 
 const students = [
-    {id:1, name: "Nur Al Mumit"},
-    {id:2, name: "Mahfuza akhter"},
-    {id:3, name: "Anika yesmin"},
-    {id:4, name: "Farzana Yeamin"},
-    {id:5, name: "Ali MD. Sohel"},
+    { id: 1, name: "Nur Al Mumit" },
+    { id: 2, name: "Mahfuza akhter" },
+    { id: 3, name: "Anika yesmin" },
+    { id: 4, name: "Farzana Yeamin" },
+    { id: 5, name: "Ali MD. Sohel" },
 ];
 
-const server = createServer((req, res)=>{
+const server = createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
     if (req.url === '/api/students' && req.method === 'GET') {
-        res.setHeader('Content-Type', 'application/json');
         res.write(JSON.stringify(students));
-        res.end();
+
+    } else if (req.url.match(/\/api\/student\/([0-9]+)/) && req.method === 'GET') {
+        let id = req.url.split('/')[3];
+        let student = students.find((student) => student.id === parseInt(id));
+        if (student) {
+            res.write(JSON.stringify(student));
+        } else {
+            res.statusCode = 404;
+            res.write(JSON.stringify({ message: "Student Not Found" }))
+        }
     } else {
-        res.setHeader('Content-Type', 'application/json');
         res.statusCode = 404;
-        res.write(JSON.stringify({message: 'Route Not Found'}));
-        res.end();
+        res.write(JSON.stringify({ message: 'Route Not Found' }));
     }
+    res.end();
 })
 
 server.listen(PORT, () => {
